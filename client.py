724706384a889f459e567d99c7b725ee707ab545
Nikita Arinkin <nikita.arinkin@gmail.com>
2018-05-05 23:58:24 +0300

    Fix size of string to length of bytes

--- client.py
+++ client.py
@@ -6,5 +6,6 @@ host = 'localhost'
 port = 6060
 string = sys.argv[1]
 s.connect((host, port))
-s.send(sys.getsizeof(string).to_bytes(4,byteorder='little')+string.encode('utf-8'))
+bytes = string.encode('utf-8')
+s.send(len(bytes).to_bytes(4,byteorder='little')+string.encode('utf-8'))
 s.close()
