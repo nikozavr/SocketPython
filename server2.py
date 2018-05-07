fda468cfe9a096f00f391b4e58643a0be0c05b0a
Nikita Arinkin <nikita.arinkin@gmail.com>
2018-05-07 16:26:21 +0300

    Add simple server on thread

---
+++ server2.py
@@ -0,0 +1,32 @@
+import socket
+from time import sleep
+import threading
+import queue
+import asyncore
+
+def process(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host,port))
+    s.listen(1)
+    global q
+    while True:
+        c, addr = s.accept()
+        size = c.recv(4)
+        data = c.recv(int.from_bytes(size, byteorder='little'))
+        c.close
+        q.put(data.decode())
+
+host = ''
+port = 6060
+q = queue.Queue()
+thread = threading.Thread(target=process, args=(host, port))
+thread.daemon = True
+thread.start()
+
+while True:
+    try:
+        print(q.get(timeout=1))
+    except KeyboardInterrupt:
+        break
+    except queue.Empty:
+        pass
